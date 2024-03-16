9276a7637d16e484ea89b55c7e85dff4d2410e21
LOKESH RAM <90214872+Lokeshram12@users.noreply.github.com>
2024-03-16 15:54:27 +0530

    Form Validation

---
+++ Forms/src/App.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (formData.name.trim() === '' || !validateEmail(formData.email)) {
+      console.log('Please enter valid name and email.');
+    } else {
+      console.log('Form submitted:', formData);
+    }
+  };
+
+  const validateEmail = (email) => {
+    //validation regex
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  return (
+    <div className="App">
+      <div>This is a form
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
